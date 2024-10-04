7621ee90eefd260adc57f76d43f5b91635aa8ccb
Uncp1 <113035612+Uncp1@users.noreply.github.com>
2024-10-04 18:35:48 +0300

    project init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="text-pretty p-10 text-center">
+      <h1 className="text-4xl font-bold">Rick and Morty Character Search</h1>
+      <p className="mt-4 text-lg">Start searching for characters!</p>
+    </div>
+  );
+}
+
+export default App;
